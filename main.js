5d234308228f2a916266f80a0c6a48ada087a544
Minahil shafique <minahal.shafique@gmail.com>
2021-04-01 13:33:33 -0400

    list commited

--- main.js
+++ main.js
@@ -16,9 +16,11 @@
 const inputText = document.querySelector("#translator-input")
 const button = document.querySelector("#addButton")
 const RemoveAll = document.querySelector("#RemoveAll")
-const olist = document.querySelector(".todo-list");
+const RemoveComplete = document.querySelector("#RemoveComplete")
 
+const olist = document.querySelector(".todo-list")
 
+const colorArray = ["list-group-item-warning" ,"list-group-item-danger","list-group-item-success"]
 // A function that, given a todo object, adds an item to our todo list array.
 function AddToDoTask(obj) {
   todos.push(obj)
@@ -45,33 +47,32 @@ const printTodo = function (todo) {
   if (todo.complete === true) {
     listItem.classList.add("complete");
   }
+  listItem.classList.add(colorArray[Math.floor(Math.random() * 3)])
   // Query the todo list <ol> and store it in a variable
-
   // Append the li we made to the ol as the last child using `.appendChild`. If this isn't working for you, check what is being appended to what!
   olist.appendChild(listItem)
+  listItem.addEventListener('click', function (event){
+    ListsClicked(event.target)
+  });
 }
 
 
 // A function that print ALL todos. It should loop through our todos array and call the above print-one-todo function on each one.
 
 function print(todoArr) {
-  console.log("print")
   for (const each of todoArr) {
     printTodo(each)
   }
 }
 
 // Now here in the global code, call the above function, so our todos array gets printed out on page load (which is when global code is run). This is the only time we're calling a function ourselves; the rest is event listeners and helper functions that run when the user interacts with the DOM!
-print(todos)
+ print(todos)
 // A function that clears all todos from the DOM. This is a great helper function for refreshing our todos.
-
-
 
 // Test it in the console and see if your list disappears!
 function refresh() {
-  todos.splice(0);
   olist.innerHTML = ''
-  print(todos)
+  todos.splice(0);
 }
 
 // A function that refreshes our page by calling each of the two above functions. Since printing all todos onto the DOM is based on our todos array, if we make a change to our todos array, we can simply call this function, which will make our DOM match our todos array by simply clearing the page and repopulating it according to our todos' new state.
@@ -89,7 +90,7 @@ Let's wire it all together. Add an event listener for the add todo button that w
 button.addEventListener('click', ButtonClicked)
 
 function ButtonClicked() {
-  olist.innerHTML = ''
+  todos.splice(0);
   let obj = { text: inputText.value, complete: false, priority: 2, id: 0 }
   AddToDoTask(obj)
   print(todos)
@@ -99,7 +100,21 @@ function ButtonClicked() {
 /*
  Run over to the HTML and add a button for CLEAR TODOS or REMOVE TODOS or some such, giving it a class or id of your choice. Now let's wire up that button, giving it a click event listener that clears all todos from the DOM (we have a function for that!) and removes all todo objects from the todos array as well.
 */
+
 RemoveAll.addEventListener('click', refresh)
+RemoveComplete.addEventListener('click', showIncomplete)
+// Stretch Goals
+function ListsClicked(obj){
+  obj.classList.add("complete")
+  obj.complete = true
+}
+function showIncomplete(){
+  olist.innerHTML = ''
+  for (const each of todos){
+   if (each.complete === false){
+    printTodo(each)
+   }
+  }
+}
 
 
-// And you're DONE with the best interface we've written yet for a todos app!
